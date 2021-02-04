42bb14f37091a48a6f6e7b5e87ead28965354034
elgar23 <superel2303@gmail.com>
2021-02-04 18:23:14 +0200

    изменил пути

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './styles.css';
+import changeTheme from './js/changeTheme'
+import './images/sprite.svg'
+import './css/styles.css';
+import './js/lazy.js'
+import menu from './store/menu.json';
+import newStringEl from './js/addMarkup.js'
+
+
+
+
+newStringEl(menu)
+changeTheme()
